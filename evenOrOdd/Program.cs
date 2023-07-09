c5672ea8a3c25a1a33508aa8addff36102cdf700
Magda <magdagvirjishvili@gmail.com>
2023-07-10 00:13:47 +0400

    magdas_commit

---
+++ evenOrOdd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("determining if a number  is even or odd");
+
+        Console.Write("Enter the number  ");
+        int number = int.Parse(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number you entered is even");
+        }
+
+        else 
+            Console.WriteLine("The number you entered is odd");
+    }
+}
